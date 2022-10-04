6f94141eb0b141e9c06bb8f47d79e048aaaa8a1d
Justin Lamb <justinlamb@me.com>
2022-10-04 14:16:59 -0700

    add obsidianpath

--- daily_note_cleanup.rb
+++ daily_note_cleanup.rb
@@ -2,15 +2,17 @@
 
 require 'fileutils'
 
-TEMPLATE_PATH = '/Users/justinlamb/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian/Templates/DailyNoteTemplate.md'
-ARCHIVE_PATH = '/Users/justinlamb/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian/Daily Notes/Archive'
+OBSIDIAN_PATH = '/Users/justinlamb/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian/'
+TEMPLATE_PATH = "#{OBSIDIAN_PATH}Templates/DailyNoteTemplate.md"
+ARCHIVE_PATH = "#{OBSIDIAN_PATH}Daily Notes/Archive"
 TRASH_PATH = '/Users/justinlamb/.Trash/'
-IGNORE_LINE = ['{{date:dddd, MMMM Do YYYY}}', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].freeze
+IGNORE_LINE = ['{{date:dddd, MMMM Do YYYY}}', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
+               'Sunday'].freeze
 
 template = []
 File.foreach(TEMPLATE_PATH) { |line| template << line unless line.match? Regexp.union(IGNORE_LINE) }
 
-notes = Dir['/Users/justinlamb/Library/Mobile Documents/iCloud~md~obsidian/Documents/Obsidian/Daily Notes/*.md'].select
+notes = Dir["#{OBSIDIAN_PATH}Daily Notes/*.md"].select
 
 notes.each do |file|
   note = []
